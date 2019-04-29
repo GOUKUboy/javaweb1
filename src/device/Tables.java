a72c727b8a5d4644298a043808c7a2a7c9ba965d
GOUKUboy <1617786925@qq.com>
2019-04-29 20:54:52 +0800

    Initial commit

---
+++ src/device/Tables.java
@@ -0,0 +1,217 @@
+package com.sample;
+
+
+public class Tables {
+
+  private String tableCatalog;
+  private String tableSchema;
+  private String tableName;
+  private String tableType;
+  private String engine;
+  private long version;
+  private String rowFormat;
+  private long tableRows;
+  private long avgRowLength;
+  private long dataLength;
+  private long maxDataLength;
+  private long indexLength;
+  private long dataFree;
+  private long autoIncrement;
+  private java.sql.Timestamp createTime;
+  private java.sql.Timestamp updateTime;
+  private java.sql.Timestamp checkTime;
+  private String tableCollation;
+  private long checksum;
+  private String createOptions;
+  private String tableComment;
+
+
+  public String getTableCatalog() {
+    return tableCatalog;
+  }
+
+  public void setTableCatalog(String tableCatalog) {
+    this.tableCatalog = tableCatalog;
+  }
+
+
+  public String getTableSchema() {
+    return tableSchema;
+  }
+
+  public void setTableSchema(String tableSchema) {
+    this.tableSchema = tableSchema;
+  }
+
+
+  public String getTableName() {
+    return tableName;
+  }
+
+  public void setTableName(String tableName) {
+    this.tableName = tableName;
+  }
+
+
+  public String getTableType() {
+    return tableType;
+  }
+
+  public void setTableType(String tableType) {
+    this.tableType = tableType;
+  }
+
+
+  public String getEngine() {
+    return engine;
+  }
+
+  public void setEngine(String engine) {
+    this.engine = engine;
+  }
+
+
+  public long getVersion() {
+    return version;
+  }
+
+  public void setVersion(long version) {
+    this.version = version;
+  }
+
+
+  public String getRowFormat() {
+    return rowFormat;
+  }
+
+  public void setRowFormat(String rowFormat) {
+    this.rowFormat = rowFormat;
+  }
+
+
+  public long getTableRows() {
+    return tableRows;
+  }
+
+  public void setTableRows(long tableRows) {
+    this.tableRows = tableRows;
+  }
+
+
+  public long getAvgRowLength() {
+    return avgRowLength;
+  }
+
+  public void setAvgRowLength(long avgRowLength) {
+    this.avgRowLength = avgRowLength;
+  }
+
+
+  public long getDataLength() {
+    return dataLength;
+  }
+
+  public void setDataLength(long dataLength) {
+    this.dataLength = dataLength;
+  }
+
+
+  public long getMaxDataLength() {
+    return maxDataLength;
+  }
+
+  public void setMaxDataLength(long maxDataLength) {
+    this.maxDataLength = maxDataLength;
+  }
+
+
+  public long getIndexLength() {
+    return indexLength;
+  }
+
+  public void setIndexLength(long indexLength) {
+    this.indexLength = indexLength;
+  }
+
+
+  public long getDataFree() {
+    return dataFree;
+  }
+
+  public void setDataFree(long dataFree) {
+    this.dataFree = dataFree;
+  }
+
+
+  public long getAutoIncrement() {
+    return autoIncrement;
+  }
+
+  public void setAutoIncrement(long autoIncrement) {
+    this.autoIncrement = autoIncrement;
+  }
+
+
+  public java.sql.Timestamp getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(java.sql.Timestamp createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public java.sql.Timestamp getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(java.sql.Timestamp updateTime) {
+    this.updateTime = updateTime;
+  }
+
+
+  public java.sql.Timestamp getCheckTime() {
+    return checkTime;
+  }
+
+  public void setCheckTime(java.sql.Timestamp checkTime) {
+    this.checkTime = checkTime;
+  }
+
+
+  public String getTableCollation() {
+    return tableCollation;
+  }
+
+  public void setTableCollation(String tableCollation) {
+    this.tableCollation = tableCollation;
+  }
+
+
+  public long getChecksum() {
+    return checksum;
+  }
+
+  public void setChecksum(long checksum) {
+    this.checksum = checksum;
+  }
+
+
+  public String getCreateOptions() {
+    return createOptions;
+  }
+
+  public void setCreateOptions(String createOptions) {
+    this.createOptions = createOptions;
+  }
+
+
+  public String getTableComment() {
+    return tableComment;
+  }
+
+  public void setTableComment(String tableComment) {
+    this.tableComment = tableComment;
+  }
+
+}
---
+++ src/device/domain/Device.java
@@ -0,0 +1,4 @@
+package device.domain;
+
+public class Device {
+}
